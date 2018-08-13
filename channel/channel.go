b7acd6c6f2b114565d3dd5d710f6cef9fa284297
jiaxuanzhou <a3721659a@hotmail.com>
2018-08-13 10:54:59 +0800

    add case in channel

--- channel/channel.go
+++ channel/channel.go
@@ -3,9 +3,39 @@ package main
 import (
 	"fmt"
 	"time"
+	"reflect"
+	"unsafe"
 
 	"k8s.io/apimachinery/pkg/util/wait"
 )
+
+func isChanClosed(ch interface{}) bool {
+	if reflect.TypeOf(ch).Kind() != reflect.Chan {
+		panic("only channels!")
+	}
+
+	// get interface value pointer, from cgo_export
+	// typedef struct { void *t; void *v; } GoInterface;
+	// then get channel real pointer
+	cptr := *(*uintptr)(unsafe.Pointer(
+		unsafe.Pointer(uintptr(unsafe.Pointer(&ch)) + unsafe.Sizeof(uint(0))),
+	))
+
+	// this function will return true if chan.closed > 0
+	// see hchan on https://github.com/golang/go/blob/master/src/runtime/chan.go
+	// type hchan struct {
+	// qcount   uint           // total data in the queue
+	// dataqsiz uint           // size of the circular queue
+	// buf      unsafe.Pointer // points to an array of dataqsiz elements
+	// elemsize uint16
+	// closed   uint32
+	// **
+
+	cptr += unsafe.Sizeof(uint(0))*2
+	cptr += unsafe.Sizeof(unsafe.Pointer(uintptr(0)))
+	cptr += unsafe.Sizeof(uint16(0))
+	return *(*uint32)(unsafe.Pointer(cptr)) > 0
+}
 
 func chanA(stopC <-chan struct{}) {
 	go func(stopCh <- chan struct{}) {
@@ -20,7 +50,7 @@ func chanA(stopC <-chan struct{}) {
 	fmt.Println("recieved stop signal")
 }
 
-func main() {
+func testC() {
 
 	testC := make(chan struct{})
 	hangC := make(chan struct{})
@@ -30,4 +60,33 @@ func main() {
 	}()
 	chanA(testC)
 	<- hangC
+}
+
+func testChanClosed(ch chan struct{}) bool {
+	fmt.Println("test")
+	ch <- struct{}{}
+	if len(ch) == 0 {
+		select {
+		case _, ok := <-ch:
+			return !ok
+		}
+	}
+	return false
+}
+
+func IsClosed(ch <-chan struct{}) bool {
+	select {
+	case <-ch:
+		return true
+	default:
+	}
+
+	return false
+}
+
+func main() {
+	c := make(chan struct{})
+	fmt.Println(IsClosed(c)) // false
+	close(c)
+	fmt.Println(IsClosed(c)) // true
 }
